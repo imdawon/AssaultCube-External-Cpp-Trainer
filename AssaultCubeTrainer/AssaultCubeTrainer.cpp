6511a44e9273913008a1ecdbb2a929c96d7226c5
dhens <dawsonhensel@gmail.com>
2020-06-22 12:30:04 -0700

    init commit. completed Proc.cpp and Proc.h to scan through process snapshot

---
+++ AssaultCubeTrainer/AssaultCubeTrainer.cpp
@@ -0,0 +1,19 @@
+// AssaultCubeTrainer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // get procid of target program
+
+    //get,modulebaseaddress
+
+    //get  handle to process
+
+    // resolve base address of the pointer chain
+
+    //resolve our ammo pointer chain
+
+     
+}
---
+++ AssaultCubeTrainer/Proc.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+DWORD GetProcId(const wchar_t* procName);
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
+
+uintptr_t findDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) {
